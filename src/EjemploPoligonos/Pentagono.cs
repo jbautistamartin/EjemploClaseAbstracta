ee09ac2e7a833c0d35a4063706dc3b277818cd0a
José Luis Bautista Martín <joseluisbautista@gmail.com>
2022-08-14 23:46:45 -0500

    Ejemplo Poligonos

---
+++ src/EjemploPoligonos/Pentagono.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploPoligonos
+{
+    class Pentagono : Poligono
+    {
+        private int longitud;
+        private int apotema;
+        
+        public Pentagono (int longitud, int apotema) : base(5)
+        {
+            this.longitud = longitud;
+            this.apotema = apotema;
+        }
+
+        public int Longitud
+        {
+            get
+            {
+                return longitud;
+            }
+
+            private set
+            {
+                longitud = value;
+            }
+        }
+
+        public int Apotema
+        {
+            get
+            {
+                return apotema;
+            }
+
+            private set
+            {
+                apotema = value;
+            }
+        }
+        public override double CalcularArea()
+        {
+            return ((5 * longitud) * apotema) / 2;
+        }
+    }
+}
---
+++ src/EjemploPoligonos/Triangulo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploPoligonos
+{
+    internal class Triangulo : Poligono
+    {
+        private int altura;
+        private int tamanoBase;
+
+        public Triangulo(int tamanoBase, int altura) : base(3)
+        {
+            this.tamanoBase = tamanoBase;
+            this.altura = altura;
+        }
+
+        public int Base
+        {
+            get
+            {
+                return tamanoBase;
+            }
+
+            private set
+            {
+                tamanoBase = value;
+            }
+        }
+
+        public int Altura
+        {
+            get
+            {
+                return altura;
+            }
+
+            private set
+            {
+                altura = value;
+            }
+        }
+
+
+        public override double CalcularArea()
+        {
+            return (tamanoBase * altura) / 2;
+        }
+    }
+}
